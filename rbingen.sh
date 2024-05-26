660dd4158d56915811b7260d84b100c9e6283068
Professor Marcos <60246107+SoykaffAddict@users.noreply.github.com>
2024-05-26 05:01:56 +0000

    Add files via upload

---
+++ rbingen.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+BS="5K"			#Size of the archive for dd (1K, 2M, 3G...)
+LIMIT=100		#Quantity of files to generate
+LOCATION="./"	#Locations where the files will be created
+
+for i in $(seq $LIMIT); do
+	dd if="/dev/urandom" bs="$BS" count="1" of="$LOCATION$i.bin"
+done
+
+chmod +x *
+
+for i in $(seq $LIMIT); do
+	clear
+	./"$i.bin"
+	sleep 2
+done
